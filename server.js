139f5763fe3986098746e2f6b348314eee00d970
Jeseekia Vaughn <jeseekia@gmail.com>
2024-01-28 22:51:46 -0500

    Reinstall jest with MongoDB compatible preset

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Come get some love!!!!')
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is running port ${port}`)
-})
+});
